345fa89165adb22e1c7e3a3f16d2328d593d304e
Jack Kelly <jack.kelly@data61.csiro.au>
2019-11-20 12:08:08 +1000

    Rerun cabal2nix

--- reflex-dom-datepicker.nix
+++ reflex-dom-datepicker.nix
@@ -1,13 +1,12 @@
-{ mkDerivation, base, containers, ghcjs-dom, jsaddle-warp, lens
-, reflex, reflex-dom, stdenv, text, time, wai-app-static
+{ mkDerivation, base, containers, jsaddle-warp, lens, reflex
+, reflex-dom-core, stdenv, text, time
 }:
 mkDerivation {
   pname = "reflex-dom-datepicker";
   version = "0.1.0";
   src = ./.;
   libraryHaskellDepends = [
-    base containers ghcjs-dom jsaddle-warp lens reflex reflex-dom text
-    time wai-app-static
+    base containers jsaddle-warp lens reflex reflex-dom-core text time
   ];
   description = "A date picker UI widget for the reflex FRP platform";
   license = stdenv.lib.licenses.bsd3;
